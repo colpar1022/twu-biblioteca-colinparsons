a221c383cdb3f4926f688e5fc0f83608322be02c
Colin Parsons <rig1996@hotmail.com>
2019-06-01 14:07:06 -0500

    Added the main menu

--- src/com/twu/biblioteca/BibliotecaApp.java
+++ src/com/twu/biblioteca/BibliotecaApp.java
@@ -3,6 +3,7 @@ package com.twu.biblioteca;
 public class BibliotecaApp {
 
     public static void main(String[] args) {
-        System.out.println("Hello, world! Hello test.");
+        System.out.println("\nWelcome to the Biblioteca. \nYour one-stop-shop for great book titles in Bangalore!\n");
+        System.out.println("Main Menu:\n1. Book List\n2. Exit Program");
     }
 }
