163485ee5fe40d804758a581426bbe36708cf777
Colin Parsons <rig1996@hotmail.com>
2019-06-01 14:29:35 -0500

    Added user interaction to view book list and exit program

--- src/com/twu/biblioteca/BibliotecaApp.java
+++ src/com/twu/biblioteca/BibliotecaApp.java
@@ -1,9 +1,37 @@
+/**
+ * Author: Colin Parsons
+ * Biblioteca Main
+ * Version: 0.2
+ * Copyright
+ */
 package com.twu.biblioteca;
+import java.util.Scanner;
 
 public class BibliotecaApp {
 
     public static void main(String[] args) {
+        Scanner userOption = new Scanner(System.in);
+        boolean bail = false;
+
         System.out.println("\nWelcome to the Biblioteca. \nYour one-stop-shop for great book titles in Bangalore!\n");
         System.out.println("Main Menu:\n1. Book List\n2. Exit Program");
+
+        while(!bail) {
+            System.out.println("\nEnter the corresponding number for what you would like to do: ");
+            int optionNum = userOption.nextInt();
+
+            switch (optionNum) {
+                case 1:
+                    System.out.println("User requested Book List");
+                    break;
+                case 2:
+                    System.out.println("User requested to end program");
+                    bail = true;
+                    break;
+                default:
+                    System.out.println("Please select a valid option!");
+            }
+        }
+        userOption.close();
     }
 }
