f3b81377283141a08401b0f3ef4ddc9dd1c1b309
Colin Parsons <rig1996@hotmail.com>
2019-06-02 18:17:56 -0500

    Output of Book List is now in table format

--- src/com/twu/biblioteca/BookList.java
+++ src/com/twu/biblioteca/BookList.java
@@ -11,29 +11,33 @@ public class BookList implements ItemList {
     private Book e = new Book();
 
     public BookList(){
+        //Because there is no actual database, the example books are set here...
         a.setTitle("Metro 2033"); a.setAuthor("Dmitry Glukhovsky"); a.setPublishDate(2003);
         b.setTitle("The Graveyard Book"); b.setAuthor("Neil Gaiman"); b.setPublishDate(2008);
         c.setTitle("The Lorax"); c.setAuthor("Dr. Seuss"); c.setPublishDate(1971);
         d.setTitle("The Elementals"); d.setAuthor("C. Parsons"); d.setPublishDate(2018);
         e.setTitle("The BFG"); e.setAuthor("Roald Dahl"); e.setPublishDate(1982);
-
-
+        //And added here...
         BList.add(a);
         BList.add(b);
         BList.add(c);
         BList.add(d);
         BList.add(e);
-
-        System.out.println("Book list was instantiated.");
     }
 
     public void displayList() {
+        System.out.println("------------------------------------------------------------------------------------------");
+        System.out.printf("%5s %30s %30s %10s %5s", "OPTION", "TITLE", "AUTHOR", "PUBLISHED", "AVAILABLE");
+        System.out.println("\n------------------------------------------------------------------------------------------");
         for(int i = 0; i < BList.size(); i++) {
             Book toShow = BList.get(i);
-            System.out.println(toShow.getTitle());
-            System.out.println(toShow.getAuthor());
-            System.out.println(toShow.getPublishDate());
-            System.out.println(toShow.getIsCheckedOut());
+            System.out.format("%5d %30s %30s %10d %10b", i, toShow.getTitle(), toShow.getAuthor(), toShow.getPublishDate(),
+                    toShow.getIsCheckedOut());
+            System.out.println();
+            //System.out.println("Title: " + toShow.getTitle());
+            //System.out.println("Author: " + toShow.getAuthor());
+            //System.out.println("Publish Date: " + toShow.getPublishDate());
+            //System.out.println("Is checked out: " + toShow.getIsCheckedOut() + "\n");
         }
     }
 }
