22f239b181e9108fb852cdd24aebb8cae9c7b2fe
Colin Parsons <rig1996@hotmail.com>
2019-06-02 14:34:40 -0500

    Added utility functions for Book class

--- src/com/twu/biblioteca/Book.java
+++ src/com/twu/biblioteca/Book.java
@@ -6,6 +6,10 @@ public class Book {
     private int publishDate;
     private Boolean isCheckedOut;
 
+    public Book(){
+        isCheckedOut = false;
+    }
+
     public String getTitle(){
         return title;
     }
@@ -14,5 +18,34 @@ public class Book {
     }
     public int getPublishDate(){
         return publishDate;
+    }
+
+    public void setTitle(String newTitle){
+        if(newTitle.length() < 1){
+            System.out.println("Title has been left blank, please enter the title name.");
+        }else{
+            title = newTitle;
+        }
+    }
+    public void setAuthor(String newAuthor){
+        if(newAuthor.length() < 1){
+            System.out.println("Author has been left blank, please enter the author's name.");
+        }else{
+            author = newAuthor;
+        }
+    }
+    public void setPublishDate(int newPublishDate){
+        if(newPublishDate < 0){
+            System.out.println("Publish date cannot be negative, enter correct publish date.");
+        }else{
+            publishDate = newPublishDate;
+        }
+    }
+    public void setIsCheckedOut(Boolean checkedOut){
+        if(checkedOut == isCheckedOut){
+            System.out.println("Status of availability has not changed.");
+        }else{
+            isCheckedOut = checkedOut;
+        }
     }
 }
