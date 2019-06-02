2ca5fe039eff2c634ed25e249533bb729de5ee03
Colin Parsons <rig1996@hotmail.com>
2019-06-02 14:15:53 -0500

    Added Book class, successfully integrated, past issue resolved

--- src/com/twu/biblioteca/Book.java
+++ src/com/twu/biblioteca/Book.java
@@ -1,5 +1,18 @@
 package com.twu.biblioteca;
 
 public class Book {
-    System.out.println("cheese");
+    private String title;
+    private String author;
+    private int publishDate;
+    private Boolean isCheckedOut;
+
+    public String getTitle(){
+        return title;
+    }
+    public String getAuthor(){
+        return author;
+    }
+    public int getPublishDate(){
+        return publishDate;
+    }
 }
